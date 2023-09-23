5bf76b5389f29f840c627ba18e25064cd1c1c812
HenriqueContini <henrique.coutine@gmail.com>
2023-09-22 21:34:22 -0300

    Chore: nav icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,42 +6,40 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Header() {
   return (
-    <Container maxWidth={false} component="header">
-      <AppBar sx={{ paddingInline: "30px" }}>
+    <Container maxWidth={false} component="header" disableGutters={true}>
+      <AppBar position="relative">
         <Toolbar
           sx={{
             padding: "30px",
             display: "flex",
-            flexDirection: { xs: "column", sm: "row" },
+            flexDirection: "row",
             gap: "30px",
             justifyContent: "space-between",
           }}
         >
           <Typography align="center" component="h1" variant="h6">
-            Henrique Contini
+            Portfólio
           </Typography>
 
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            spacing={{ xs: 2, sm: 8 }}
-            component="nav"
-          >
+          <Stack direction="row" spacing={{ xs: 3, sm: 5 }} component="nav">
             <Link
               href="https://github.com/HenriqueContini"
               underline="hover"
               align="center"
             >
-              GitHub
+              <GitHubIcon fontSize="large" />
             </Link>
             <Link
               href="https://www.linkedin.com/in/henrique-contini-83a149215/"
               underline="hover"
               align="center"
             >
-              LinkedIn
+              <LinkedInIcon fontSize="large" />
             </Link>
           </Stack>
         </Toolbar>
